61957de2dedbc6730f645f353ee0f12ed39e75cd
Ali <ali.moaydi2020@gmail.com>
2023-04-29 02:43:57 +0430

    3. Firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { getAuth , signInWithRedirect , getRedirectResult , GoogleAuthProvider } from 'firebase/auth';
+import {app} from '../firebase';
 
 // Icons
 import googleLogo from '../assets/icons/Google__G__Logo.svg.png';
@@ -6,11 +8,30 @@ import googleLogo from '../assets/icons/Google__G__Logo.svg.png';
 import styles from '../styles/Login.module.css';
 
 export default function Login() {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  getRedirectResult(auth)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access Google APIs.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+  
+      // The signed-in user info.
+      const user = result.user;
+     
+      console.log(user);
+    })
+
+  const clickHandler = () => {
+    signInWithRedirect(auth , provider);
+
+  }
   return (
     <div className={styles.container}>
       <div className={styles.loginCard}>
         <h2>Welcome To Sou Online!</h2>
-        <div className={styles.button}>
+        <div className={styles.button} onClick={clickHandler}>
           <img src={googleLogo} alt="Google" /> Sing in With Google
         </div>
       </div>
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+    apiKey: "AIzaSyDEtp49Sa2E7yvLmMmwjloSaciHKMHvxO4",
+    authDomain: "sou-online.firebaseapp.com",
+    projectId: "sou-online",
+    storageBucket: "sou-online.appspot.com",
+    messagingSenderId: "652086452707",
+    appId: "1:652086452707:web:08973083650d1e551e80d6"
+  };
+
+export const app = initializeApp(firebaseConfig);
